7093f282c214e08fa4c493ce3998a32adb6cb05b
Leonardo Gatica <lgatica@protonmail.com>
2018-11-15 14:18:22 -0300

    fix(raven): add dsn

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ Vue.config.productionTip = false
 
 if (process.env.NODE_ENV === 'production') {
   Vue.use(VueRaven, {
-    dsn: process.env.SENTRY_DSN_PUBLIC
+    dsn: 'https://7f31a740e7ec460cbdd1c9fdf2595be0@sentry.io/1323839'
   })
 }
 
