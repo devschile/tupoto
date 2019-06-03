f7b0425ab2e616bcd1d9a5df5746a5f3cc118a86
Leonardo Gatica <lgatica@protonmail.com>
2019-06-02 23:14:16 -0400

    fix(functions): fix logger

--- functions/utils/logger.js
+++ functions/utils/logger.js
@@ -1,12 +1,7 @@
 'use strict'
 
 const { createLogger, transports } = require('winston')
-const SentryTransport = require('winston-sentry-node')
-const { sentry } = require('./config')
 
-const customTransports = [new transports.Console({ timestamp: true })]
-if (process.env.SENTRY_DSN) {
-  customTransports.push(new SentryTransport({ sentry }))
-}
-
-module.exports = createLogger({ transports: customTransports })
+module.exports = createLogger({
+  transports: [new transports.Console({ timestamp: true })]
+})
