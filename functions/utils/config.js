a0b854ed5a7224b5719f30b87907aa4c1d691d0f
Leonardo Gatica <lgatica@protonmail.com>
2019-06-02 23:10:35 -0400

    fix(functions): fix call package.json

--- functions/utils/config.js
+++ functions/utils/config.js
@@ -1,17 +1,12 @@
 'use strict'
 
 const dburi = require('config-dburi')
-const pkg = require('../../package.json')
 
 module.exports = {
   redis: dburi.redis(),
   expire: process.env.EXPIRE || 60 * 24 * 60 * 60, // expire in 60 days
-  version: pkg.version,
   uidLength: process.env.UID_LENGTH || 4,
   sentry: {
-    dsn: process.env.SENTRY_DSN,
-    config: {
-      release: pkg.version
-    }
+    dsn: process.env.SENTRY_DSN
   }
 }
